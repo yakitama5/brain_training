dc7c54f1b5b8ef5c55752406eb585fdfbcbcbedd
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-03-25 15:56:22 +0900

    🚧wip: 音声入力の検証

--- lib/src/presentation/pages/sample_page.dart
+++ lib/src/presentation/pages/sample_page.dart
@@ -5,14 +5,28 @@ import 'package:brain_training/src/domain/read_color/value_object/colored_word.d
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 import 'colored_word/components/mixed_colored_word_text.dart';
 
 class SamplePage extends HookConsumerWidget {
-  const SamplePage({super.key});
+  SamplePage({super.key});
+
+  final SpeechToText speech = SpeechToText();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final text = useState('');
+    useEffect(
+      () {
+        onListen((result) {
+          text.value = result.recognizedWords;
+        });
+        return speech.cancel;
+      },
+      [speech],
+    );
+
     final word = useState(_createMixedWord());
 
     return Scaffold(
@@ -41,6 +55,13 @@ class SamplePage extends HookConsumerWidget {
     return MixedColoredWord(
       color: ColoredWord.values[Random().nextInt(max)],
       word: ColoredWord.values[Random().nextInt(max)],
+    );
+  }
+
+  Future<void> onListen(SpeechResultListener onResult) async {
+    await speech.initialize();
+    await speech.listen(
+      onResult: onResult,
     );
   }
 }
