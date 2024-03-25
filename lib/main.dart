4a4ee316c6d3ec2f2359e6cc3a29f6c187f2a788
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-03-25 11:29:20 +0900

    🔧chore: riverpodを利用するために共通宣言を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'src/app.dart';
 
-void main() {
-  runApp(const App());
-}
+void main() => runApp(const ProviderScope(child: App()));
