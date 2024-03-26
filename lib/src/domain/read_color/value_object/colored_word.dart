943891aa9da21e94ebed1e549108e892b06c777d
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-03-26 15:37:02 +0900

    ✨feat: 色当てクイズの選択肢を4種類に変更

--- lib/src/domain/read_color/value_object/colored_word.dart
+++ lib/src/domain/read_color/value_object/colored_word.dart
@@ -4,8 +4,8 @@ enum ColoredWord {
   black(Colors.black, 'くろ', 'くろ', 'クロ', '黒'),
   red(Colors.red, 'あか', 'あか', 'アカ', '赤'),
   blue(Colors.blue, 'あお', 'あお', 'アオ', '青'),
-  green(Colors.green, 'みどり', 'みどり', 'ミドリ', '緑'),
-  yellow(Colors.yellow, 'きいろ', 'きいろ', 'キイロ', '黄色');
+  green(Colors.green, 'みどり', 'みどり', 'ミドリ', '緑');
+  // yellow(Colors.yellow, 'きいろ', 'きいろ', 'キイロ', '黄色');
 
   const ColoredWord(
     this.color,
