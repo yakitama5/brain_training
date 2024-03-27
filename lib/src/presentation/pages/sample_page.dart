ee0b8c52808046c520f0d26b8430d925780e62c2
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-03-27 18:38:06 +0900

    🚧wip: 音声入力の検証

--- lib/src/presentation/pages/sample_page.dart
+++ lib/src/presentation/pages/sample_page.dart
@@ -1,13 +1,7 @@
-import 'dart:math';
-
-import 'package:brain_training/src/domain/read_color/entity/mixed_colored_word.dart';
-import 'package:brain_training/src/domain/read_color/value_object/colored_word.dart';
+import 'package:brain_training/src/presentation/pages/colored_word/pages/colored_word_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:speech_to_text/speech_to_text.dart';
-
-import 'colored_word/components/mixed_colored_word_text.dart';
 
 class SamplePage extends HookConsumerWidget {
   SamplePage({super.key});
@@ -16,52 +10,16 @@ class SamplePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final text = useState('');
-    useEffect(
-      () {
-        onListen((result) {
-          text.value = result.recognizedWords;
-        });
-        return speech.cancel;
-      },
-      [speech],
-    );
-
-    final word = useState(_createMixedWord());
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('SamplePage'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MixedColoredWordText(coloredWord: word.value),
-            FilledButton.icon(
-              onPressed: () => word.value = _createMixedWord(),
-              icon: const Icon(Icons.shuffle),
-              label: const Text('シャッフル'),
-            ),
-          ],
+      body: FilledButton(
+        child: const Text('ColoredWord'),
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute<void>(builder: (context) => ColoredWordPage()),
         ),
       ),
-    );
-  }
-
-  MixedColoredWord _createMixedWord() {
-    final max = ColoredWord.values.length;
-
-    return MixedColoredWord(
-      color: ColoredWord.values[Random().nextInt(max)],
-      word: ColoredWord.values[Random().nextInt(max)],
-    );
-  }
-
-  Future<void> onListen(SpeechResultListener onResult) async {
-    await speech.initialize();
-    await speech.listen(
-      onResult: onResult,
     );
   }
 }
