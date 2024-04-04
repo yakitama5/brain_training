0d2a6df417ee88b6b5cec54d57b55592e0251c7a
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-04-04 11:10:43 +0900

    🔧chore: カウントダウン用サウンドの追加

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsSoundsGen {
+  const $AssetsSoundsGen();
+
+  /// File path: assets/sounds/countdown_count.mp3
+  String get countdownCount => 'assets/sounds/countdown_count.mp3';
+
+  /// File path: assets/sounds/countdown_end.mp3
+  String get countdownEnd => 'assets/sounds/countdown_end.mp3';
+
+  /// List of all assets
+  List<String> get values => [countdownCount, countdownEnd];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsSoundsGen sounds = $AssetsSoundsGen();
+}
