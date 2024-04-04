a2791f6bcb2b03af6a8d3174bf58afbfc23a3fdf
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-04-04 10:35:02 +0900

    🎨design: slangを用いた多言語化対応

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -4,13 +4,15 @@
 /// To regenerate, run: `dart run slang`
 ///
 /// Locales: 2
-/// Strings: 54 (27 per locale)
+/// Strings: 106 (53 per locale)
 ///
-/// Built on 2024-04-03 at 07:40 UTC
+/// Built on 2024-04-04 at 01:34 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
 
+import 'package:brain_training/app/domain/training/value_object/rank_category.dart';
+import 'package:brain_training/app/domain/training/value_object/result_rank.dart';
 import 'package:brain_training/app/domain/training/value_object/training_type.dart';
 import 'package:flutter/widgets.dart';
 import 'package:slang/builder/model/node.dart';
@@ -151,6 +153,7 @@ class Translations implements BaseTranslations<AppLocale, Translations> {
 	// Translations
 	late final _StringsCommonEn common = _StringsCommonEn._(_root);
 	late final _StringsHomeEn home = _StringsHomeEn._(_root);
+	late final _StringsSettingsEn settings = _StringsSettingsEn._(_root);
 	late final _StringsTrainingEn training = _StringsTrainingEn._(_root);
 }
 
@@ -164,6 +167,7 @@ class _StringsCommonEn {
 	String get save => 'Save';
 	late final _StringsCommonNavigationDestinationEn navigationDestination = _StringsCommonNavigationDestinationEn._(_root);
 	String get start => 'Start!';
+	String get end => 'End';
 }
 
 // Path: home
@@ -180,6 +184,18 @@ class _StringsHomeEn {
 	String get completed => 'Completed';
 }
 
+// Path: settings
+class _StringsSettingsEn {
+	_StringsSettingsEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get title => '設定';
+	late final _StringsSettingsListEn list = _StringsSettingsListEn._(_root);
+	late final _StringsSettingsAccountEn account = _StringsSettingsAccountEn._(_root);
+}
+
 // Path: training
 class _StringsTrainingEn {
 	_StringsTrainingEn._(this._root);
@@ -190,6 +206,7 @@ class _StringsTrainingEn {
 	String get menu => 'Menu';
 	String get doneMenu => 'Done';
 	late final _StringsTrainingTrainingCardEn trainingCard = _StringsTrainingTrainingCardEn._(_root);
+	late final _StringsTrainingResultEn result = _StringsTrainingResultEn._(_root);
 	String get tutorial => 'Tutorial';
 }
 
@@ -205,6 +222,28 @@ class _StringsCommonNavigationDestinationEn {
 	String get training => 'Training';
 	String get coffeBreak => 'Break';
 	String get settings => 'Settings';
+}
+
+// Path: settings.list
+class _StringsSettingsListEn {
+	_StringsSettingsListEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	late final _StringsSettingsListAccountEn account = _StringsSettingsListAccountEn._(_root);
+}
+
+// Path: settings.account
+class _StringsSettingsAccountEn {
+	_StringsSettingsAccountEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get accountLink => 'アカウント連携';
+	String get other => 'その他';
+	String get leave => '退会';
 }
 
 // Path: training.trainingCard
@@ -249,6 +288,102 @@ class _StringsTrainingTrainingCardEn {
 	String get start => 'Start';
 }
 
+// Path: training.result
+class _StringsTrainingResultEn {
+	_StringsTrainingResultEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	late final _StringsTrainingResultScoreCardEn scoreCard = _StringsTrainingResultScoreCardEn._(_root);
+	late final _StringsTrainingResultRankEn rank = _StringsTrainingResultRankEn._(_root);
+	String category({required RankCategory context}) {
+		switch (context) {
+			case RankCategory.normal:
+				return 'English';
+			case RankCategory.sumo:
+				return 'Sumo';
+			case RankCategory.royal:
+				return 'Royal';
+		}
+	}
+	String questions({required int qeustions}) => '${qeustions} qeustions';
+	String score({required int points}) => '${points} points';
+	String correctRate({required int rate}) => 'Correct rate: ${rate}%';
+}
+
+// Path: settings.list.account
+class _StringsSettingsListAccountEn {
+	_StringsSettingsListAccountEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get head => 'アカウント';
+	String get account => 'アカウント';
+}
+
+// Path: training.result.scoreCard
+class _StringsTrainingResultScoreCardEn {
+	_StringsTrainingResultScoreCardEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String get title => 'Score';
+}
+
+// Path: training.result.rank
+class _StringsTrainingResultRankEn {
+	_StringsTrainingResultRankEn._(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String normal({required ResultRank context}) {
+		switch (context) {
+			case ResultRank.excellent:
+				return 'Eexcellent!!!';
+			case ResultRank.great:
+				return 'Great!';
+			case ResultRank.good:
+				return 'Good';
+			case ResultRank.average:
+				return 'Average';
+			case ResultRank.poor:
+				return 'Poor...';
+		}
+	}
+	String sumo({required ResultRank context}) {
+		switch (context) {
+			case ResultRank.excellent:
+				return 'Yokozuna!!!';
+			case ResultRank.great:
+				return 'Ozeki!';
+			case ResultRank.good:
+				return 'Sekiwake';
+			case ResultRank.average:
+				return 'Maegashira';
+			case ResultRank.poor:
+				return 'Non-banzuke...';
+		}
+	}
+	String royal({required ResultRank context}) {
+		switch (context) {
+			case ResultRank.excellent:
+				return 'Prince';
+			case ResultRank.great:
+				return 'Marquess';
+			case ResultRank.good:
+				return 'Earl';
+			case ResultRank.average:
+				return 'Viscount';
+			case ResultRank.poor:
+				return 'Baron';
+		}
+	}
+}
+
 // Path: <root>
 class _StringsJa extends Translations {
 	/// You can call this constructor and build your own translation instance of this locale.
@@ -290,6 +425,7 @@ class _StringsCommonJa extends _StringsCommonEn {
 	@override String get save => '保存';
 	@override late final _StringsCommonNavigationDestinationJa navigationDestination = _StringsCommonNavigationDestinationJa._(_root);
 	@override String get start => 'スタート！';
+	@override String get end => '終了';
 }
 
 // Path: home
@@ -316,6 +452,7 @@ class _StringsTrainingJa extends _StringsTrainingEn {
 	@override String get menu => '脳トレメニュー';
 	@override String get doneMenu => '実施済のトレーニング';
 	@override late final _StringsTrainingTrainingCardJa trainingCard = _StringsTrainingTrainingCardJa._(_root);
+	@override late final _StringsTrainingResultJa result = _StringsTrainingResultJa._(_root);
 	@override String get tutorial => 'チュートリアル';
 }
 
@@ -327,10 +464,10 @@ class _StringsCommonNavigationDestinationJa extends _StringsCommonNavigationDest
 
 	// Translations
 	@override String get home => 'ホーム';
-	@override String get analytics => 'ふりかえり';
+	@override String get analytics => '振り返り';
 	@override String get training => '脳トレ';
-	@override String get coffeBreak => 'いきぬき';
-	@override String get settings => 'せってい';
+	@override String get coffeBreak => '息抜き';
+	@override String get settings => '設定';
 }
 
 // Path: training.trainingCard
@@ -375,6 +512,91 @@ class _StringsTrainingTrainingCardJa extends _StringsTrainingTrainingCardEn {
 	@override String get start => '測定';
 }
 
+// Path: training.result
+class _StringsTrainingResultJa extends _StringsTrainingResultEn {
+	_StringsTrainingResultJa._(_StringsJa root) : this._root = root, super._(root);
+
+	@override final _StringsJa _root; // ignore: unused_field
+
+	// Translations
+	@override late final _StringsTrainingResultScoreCardJa scoreCard = _StringsTrainingResultScoreCardJa._(_root);
+	@override late final _StringsTrainingResultRankJa rank = _StringsTrainingResultRankJa._(_root);
+	@override String category({required RankCategory context}) {
+		switch (context) {
+			case RankCategory.normal:
+				return '英語';
+			case RankCategory.sumo:
+				return '相撲';
+			case RankCategory.royal:
+				return '爵位';
+		}
+	}
+	@override String questions({required int qeustions}) => '${qeustions}問';
+	@override String score({required int points}) => '${points}点';
+	@override String correctRate({required int rate}) => '正答率：${rate}%';
+}
+
+// Path: training.result.scoreCard
+class _StringsTrainingResultScoreCardJa extends _StringsTrainingResultScoreCardEn {
+	_StringsTrainingResultScoreCardJa._(_StringsJa root) : this._root = root, super._(root);
+
+	@override final _StringsJa _root; // ignore: unused_field
+
+	// Translations
+	@override String get title => '評価';
+}
+
+// Path: training.result.rank
+class _StringsTrainingResultRankJa extends _StringsTrainingResultRankEn {
+	_StringsTrainingResultRankJa._(_StringsJa root) : this._root = root, super._(root);
+
+	@override final _StringsJa _root; // ignore: unused_field
+
+	// Translations
+	@override String normal({required ResultRank context}) {
+		switch (context) {
+			case ResultRank.excellent:
+				return 'Eexcellent!!!';
+			case ResultRank.great:
+				return 'Great!';
+			case ResultRank.good:
+				return 'Good';
+			case ResultRank.average:
+				return 'Average';
+			case ResultRank.poor:
+				return 'Poor...';
+		}
+	}
+	@override String sumo({required ResultRank context}) {
+		switch (context) {
+			case ResultRank.excellent:
+				return '横綱級';
+			case ResultRank.great:
+				return '大関級';
+			case ResultRank.good:
+				return '関脇級';
+			case ResultRank.average:
+				return '前頭級';
+			case ResultRank.poor:
+				return '番付外';
+		}
+	}
+	@override String royal({required ResultRank context}) {
+		switch (context) {
+			case ResultRank.excellent:
+				return '公爵';
+			case ResultRank.great:
+				return '侯爵';
+			case ResultRank.good:
+				return '伯爵';
+			case ResultRank.average:
+				return '子爵';
+			case ResultRank.poor:
+				return '男爵';
+		}
+	}
+}
+
 /// Flat map(s) containing all translations.
 /// Only for edge cases! For simple maps, use the map function of this library.
 
@@ -388,11 +610,18 @@ extension on Translations {
 			case 'common.navigationDestination.coffeBreak': return 'Break';
 			case 'common.navigationDestination.settings': return 'Settings';
 			case 'common.start': return 'Start!';
+			case 'common.end': return 'End';
 			case 'home.todayStatus': return 'Today';
 			case 'home.todayNews': return 'News';
 			case 'home.thisWeekStatus': return 'This Week';
 			case 'home.dailyTraining': return 'Daily';
 			case 'home.completed': return 'Completed';
+			case 'settings.title': return '設定';
+			case 'settings.list.account.head': return 'アカウント';
+			case 'settings.list.account.account': return 'アカウント';
+			case 'settings.account.accountLink': return 'アカウント連携';
+			case 'settings.account.other': return 'その他';
+			case 'settings.account.leave': return '退会';
 			case 'training.menu': return 'Menu';
 			case 'training.doneMenu': return 'Done';
 			case 'training.trainingCard.title': return ({required TrainingType context}) {
@@ -428,6 +657,62 @@ extension on Translations {
 				}
 			};
 			case 'training.trainingCard.start': return 'Start';
+			case 'training.result.scoreCard.title': return 'Score';
+			case 'training.result.rank.normal': return ({required ResultRank context}) {
+				switch (context) {
+					case ResultRank.excellent:
+						return 'Eexcellent!!!';
+					case ResultRank.great:
+						return 'Great!';
+					case ResultRank.good:
+						return 'Good';
+					case ResultRank.average:
+						return 'Average';
+					case ResultRank.poor:
+						return 'Poor...';
+				}
+			};
+			case 'training.result.rank.sumo': return ({required ResultRank context}) {
+				switch (context) {
+					case ResultRank.excellent:
+						return 'Yokozuna!!!';
+					case ResultRank.great:
+						return 'Ozeki!';
+					case ResultRank.good:
+						return 'Sekiwake';
+					case ResultRank.average:
+						return 'Maegashira';
+					case ResultRank.poor:
+						return 'Non-banzuke...';
+				}
+			};
+			case 'training.result.rank.royal': return ({required ResultRank context}) {
+				switch (context) {
+					case ResultRank.excellent:
+						return 'Prince';
+					case ResultRank.great:
+						return 'Marquess';
+					case ResultRank.good:
+						return 'Earl';
+					case ResultRank.average:
+						return 'Viscount';
+					case ResultRank.poor:
+						return 'Baron';
+				}
+			};
+			case 'training.result.category': return ({required RankCategory context}) {
+				switch (context) {
+					case RankCategory.normal:
+						return 'English';
+					case RankCategory.sumo:
+						return 'Sumo';
+					case RankCategory.royal:
+						return 'Royal';
+				}
+			};
+			case 'training.result.questions': return ({required int qeustions}) => '${qeustions} qeustions';
+			case 'training.result.score': return ({required int points}) => '${points} points';
+			case 'training.result.correctRate': return ({required int rate}) => 'Correct rate: ${rate}%';
 			case 'training.tutorial': return 'Tutorial';
 			default: return null;
 		}
@@ -439,11 +724,12 @@ extension on _StringsJa {
 		switch (path) {
 			case 'common.save': return '保存';
 			case 'common.navigationDestination.home': return 'ホーム';
-			case 'common.navigationDestination.analytics': return 'ふりかえり';
+			case 'common.navigationDestination.analytics': return '振り返り';
 			case 'common.navigationDestination.training': return '脳トレ';
-			case 'common.navigationDestination.coffeBreak': return 'いきぬき';
-			case 'common.navigationDestination.settings': return 'せってい';
+			case 'common.navigationDestination.coffeBreak': return '息抜き';
+			case 'common.navigationDestination.settings': return '設定';
 			case 'common.start': return 'スタート！';
+			case 'common.end': return '終了';
 			case 'home.todayStatus': return '今日の状況';
 			case 'home.todayNews': return '今日のニュース';
 			case 'home.thisWeekStatus': return '今週の状況';
@@ -484,6 +770,62 @@ extension on _StringsJa {
 				}
 			};
 			case 'training.trainingCard.start': return '測定';
+			case 'training.result.scoreCard.title': return '評価';
+			case 'training.result.rank.normal': return ({required ResultRank context}) {
+				switch (context) {
+					case ResultRank.excellent:
+						return 'Eexcellent!!!';
+					case ResultRank.great:
+						return 'Great!';
+					case ResultRank.good:
+						return 'Good';
+					case ResultRank.average:
+						return 'Average';
+					case ResultRank.poor:
+						return 'Poor...';
+				}
+			};
+			case 'training.result.rank.sumo': return ({required ResultRank context}) {
+				switch (context) {
+					case ResultRank.excellent:
+						return '横綱級';
+					case ResultRank.great:
+						return '大関級';
+					case ResultRank.good:
+						return '関脇級';
+					case ResultRank.average:
+						return '前頭級';
+					case ResultRank.poor:
+						return '番付外';
+				}
+			};
+			case 'training.result.rank.royal': return ({required ResultRank context}) {
+				switch (context) {
+					case ResultRank.excellent:
+						return '公爵';
+					case ResultRank.great:
+						return '侯爵';
+					case ResultRank.good:
+						return '伯爵';
+					case ResultRank.average:
+						return '子爵';
+					case ResultRank.poor:
+						return '男爵';
+				}
+			};
+			case 'training.result.category': return ({required RankCategory context}) {
+				switch (context) {
+					case RankCategory.normal:
+						return '英語';
+					case RankCategory.sumo:
+						return '相撲';
+					case RankCategory.royal:
+						return '爵位';
+				}
+			};
+			case 'training.result.questions': return ({required int qeustions}) => '${qeustions}問';
+			case 'training.result.score': return ({required int points}) => '${points}点';
+			case 'training.result.correctRate': return ({required int rate}) => '正答率：${rate}%';
 			case 'training.tutorial': return 'チュートリアル';
 			default: return null;
 		}
