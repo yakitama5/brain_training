6f28d81a980d0609f40b844c5431f8aebcceda94
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-05-02 17:37:14 +0900

    🚧wip: エラー表示の出し分けを追加

--- lib/app/presentation/pages/error/components/error_view.dart
+++ lib/app/presentation/pages/error/components/error_view.dart
@@ -1,7 +1,9 @@
+import 'package:brain_training/app/application/model/flavor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
 
+import '../../../../application/config/app_config.dart';
 import '../../../../domain/exception/exceptions.dart';
 
 class ErrorView extends HookWidget {
@@ -11,9 +13,39 @@ class ErrorView extends HookWidget {
   final StackTrace? stackTrace;
 
   @override
+  Widget build(BuildContext context) =>
+      // 環境に応じて表示を切り替える
+      switch (appConfig.flavor) {
+        Flavor.dev => _DebugErrorView(error, stackTrace),
+        Flavor.prod => _ProdErrorView(error, stackTrace),
+      };
+}
+
+class SliverErrorView extends ErrorView {
+  const SliverErrorView(super.error, super.stackTrace, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverFillRemaining(
+      child: super.build(context),
+    );
+  }
+}
+
+/// 本番環境用のエラー表示
+class _ProdErrorView extends HookWidget {
+  const _ProdErrorView(this.error, this.stackTrace);
+
+  final Object error;
+  final StackTrace? stackTrace;
+
+  @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
+    /// 利用者向けにわかりやすい表示を行う
+    /// エラー情報の収集などはWidgetではなく設定で対応するため不要
+    // TODO(yakitama5): 利用者用のアニメーションとエラー表示を追加
     return SafeArea(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -42,13 +74,30 @@ class ErrorView extends HookWidget {
   }
 }
 
-class SliverErrorView extends ErrorView {
-  const SliverErrorView(super.error, super.stackTrace, {super.key});
+/// 開発環境用のエラー表示
+class _DebugErrorView extends HookWidget {
+  const _DebugErrorView(this.error, this.stackTrace);
+
+  final Object error;
+  final StackTrace? stackTrace;
 
   @override
   Widget build(BuildContext context) {
-    return SliverFillRemaining(
-      child: super.build(context),
+    final textTheme = Theme.of(context).textTheme;
+
+    // 開発者向けには単純なエラー内容を表示するのみ
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Gap(8),
+          Text(
+            error.errorMessage,
+            style: textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const Gap(40),
+        ],
+      ),
     );
   }
 }
