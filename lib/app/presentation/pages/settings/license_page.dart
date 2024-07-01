e9c8a3f1a655862a6c8b7aad8214863ffdf87400
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-07-01 23:02:28 +0900

    ✨feat: ライセンスページの追加

--- lib/app/presentation/pages/settings/license_page.dart
+++ lib/app/presentation/pages/settings/license_page.dart
@@ -1,3 +1,4 @@
+import 'package:brain_training/app/application/config/app_build_config_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -6,9 +7,12 @@ class MyLincensePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO(yakitama5): 未着手
-    return const Scaffold(
-      body: Text('ライセンス'),
+    final appBuildConfig = ref.watch(appBuildConfigProvider);
+    return LicensePage(
+      applicationName: appBuildConfig.appName,
+      applicationVersion: appBuildConfig.version,
+      applicationIcon: const Icon(Icons.flutter_dash),
+      applicationLegalese: '@Yakuran',
     );
   }
 }
